0ff846b1afe2fe8feadcb9d48abc5ac37c3db584
Victor Desire <61976018+vdeezie@users.noreply.github.com>
2021-05-11 10:49:30 +0100

    First Commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require ('express');
 const app = express();
 
 //connect application to mongodb
-const connectionString = "mongodb://localhost:27017/bookshop"
+const connectionString = "mongodb://localhost:27017/staff"
 const MongoClient = require('mongodb').MongoClient;
 
 const client = new MongoClient(connectionString, {
@@ -17,12 +17,12 @@ app.use(express.json())
 
 //CRUD 
 
-//to get the list of available books on the database
-app.get('/books', (req, res)=> {
+//to get the list of available staff on the database
+app.get('/staff', (req, res)=> {
     client.connect((err, connectedClient) => {
         if (err) return res.status(500).json({message: err});
         const db = connectedClient.db();
-        db.collection("books").find({}).toArray((err, result) => {
+        db.collection("staff").find({}).toArray((err, result) => {
             if (err) {
                 return res.status (500).json({message: err})
             }
@@ -31,28 +31,33 @@ app.get('/books', (req, res)=> {
     })
 })
 
-//Add a new book to the database
-app.post('/books', (req, res)=> {
+// //Add a new staff to the database
+app.post('/staff', (req, res)=> {
     client.connect((err, connectedClient) => {
         if (err) {
             return res.status(500).json({message: err})
         } 
         const db = connectedClient.db();
-        db.collection("books").insertOne({
-            author: req.body.author,
-            title: req.body.title
+        db.collection("staff").insertOne({
+            name: req.body.name,
+            position: req.body.position,
+            email: req.body.email,
+            country: req.body.country
         }, (err, result) => {
                 if(err) return res.status(500).json({message: err});
-                return res.status(200).json({message: "new book has been added to bookshop" })
+                return res.status(200).json({message: "new staff has been added to database" })
         })
     })
 }) 
 
+//update a staff in the database
+app.post
 
-
+//Delete a staff from the database
+app.delete
 
 
 
 
 //ask app to listen to port 5000
-app.listen(4000,() =>console.log ('server up and running'))
+app.listen(3000,() =>console.log ('server up and running'))
