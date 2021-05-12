1c0e0f0288dd94cd74597a75d32c63acd5f5cd17
Victor Desire <61976018+vdeezie@users.noreply.github.com>
2021-05-12 23:10:41 +0100

    env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //require express module
 const express = require ('express');
 const app = express();
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT == 5000; 
 
 //set up mongoose
 const mongoose = require ('mongoose');
@@ -21,14 +21,6 @@ mongoose.connect(connectionString, {
 
 //configure app to fetch data from request body
 app.use(express.json())
-if(process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('client/build'));
-}
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
-
 
 //Schema 
 const staffSchema = new mongoose.Schema ({
